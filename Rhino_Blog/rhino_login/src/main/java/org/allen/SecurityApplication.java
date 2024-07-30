22861cf4b291984c39415a614698aa62a6c4b36d
ArtistS-win <sylgg0918@gmail.com>
2024-07-30 22:43:40 +0100

    basic gradle framework build

---
+++ Rhino_Blog/rhino_login/src/main/java/org/allen/SecurityApplication.java
@@ -0,0 +1,13 @@
+package org.allen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SecurityApplication {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(SecurityApplication.class, args);
+        System.out.println("Print anything here, and add a breakpoint here.");
+    }
+}
